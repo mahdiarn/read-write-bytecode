19bdbf05727b03568596a451dd879b4af2919823
Mahdiar Naufal <intern_mahdiar@jojonomic.com>
2019-10-17 04:32:26 +0700

    bersih - bersih

--- src/FileFormat.h
+++
@@ -1,9 +0,0 @@
-#ifndef FILEFORMAT_H_INCLUDED
-#define FILEFORMAT_H_INCLUDED
-/* ^^ these are the include guards */
-
-/* Prototypes for the functions */
-
-void printFormat(unsigned char *buffer, const char * filename);
-
-#endif
