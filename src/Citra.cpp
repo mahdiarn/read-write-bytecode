fcbdd1584e5747b8a521b72d98abece73b65892b
Mahdiar Naufal <intern_mahdiar@jojonomic.com>
2019-10-17 22:31:06 +0700

    load P1 ke channel

--- src/Citra.cpp
+++ src/Citra.cpp
@@ -12,6 +12,9 @@ Citra::Citra() {
 
 void Citra::loadFile(std::vector<unsigned char> byteFile, int fileType) {
     std::vector<unsigned char> buffer;
+    std::vector<unsigned char> bufferBinary;
+    std::vector<unsigned char> singleBinary(8);
+    std::fill (singleBinary.begin(),singleBinary.begin()+8,'0');
     std::vector<std::vector<unsigned char>> bufferCitra;
     bool flagWidth = true;
     bool flagHeight = true;
@@ -25,11 +28,6 @@ void Citra::loadFile(std::vector<unsigned char> byteFile, int fileType) {
     int counter = 0;
     std::cout << "Konstruktor\n";
     std::cout << "filetype: " << fileType << std::endl;
-    for (int i = 0; i<byteFile.size(); i++) {
-        if (byteFile.at(i) == '6'){
-            std::cout << byteFile.at(i) << " ";
-        }
-    }
     switch(fileType) {
         case 1:
             this->maxValue = 1;
@@ -83,6 +81,83 @@ void Citra::loadFile(std::vector<unsigned char> byteFile, int fileType) {
                 }
             }
             this->kanal.push_back(bufferCitra);
+            break;
+        case 11:
+            this->maxValue = 1;
+            for(int i = startWidth;i<byteFile.size();i++) {
+                if (flagWidth) {
+                    if ((byteFile.at(i) >= '0') && (byteFile.at(i) <= '9')) {
+                        startWidth = i;
+                        buffer.push_back(byteFile.at(i));
+                        widthFound = true;
+                    }
+                    if (((byteFile.at(i+1) == 0x0a) || (byteFile.at(i+1) == 0x20)) && (widthFound)) {
+                        endWidth = i;
+                        flagWidth = false;
+                    }
+                }
+            }
+            for(int i = 0;i<buffer.size();i++) {
+                this->width = this->width * 10;
+                this->width = this->width + (buffer.at(i) - 48);
+            }
+            startHeight = endWidth + 1;
+            buffer.clear();
+            for(int i = startHeight;i<byteFile.size()-1;i++) {
+                if (flagHeight) {
+                    if ((byteFile.at(i) >= '0') && (byteFile.at(i) <= '9')) {
+                        buffer.push_back(byteFile.at(i));
+                        heightFound = true;
+                    }
+                    if (((byteFile.at(i+1) == 0x0a) || (byteFile.at(i+1) == 0x20)) && (heightFound)) {
+                        endHeight = i+1;
+                        flagHeight = false;
+                    }
+                }
+            }
+            for(int i = 0;i<buffer.size();i++) {
+                this->height = this->height * 10;
+                this->height = this->height + (buffer.at(i) - 48);
+            }
+            startData = endHeight + 1;
+            buffer.clear();
+            bufferBinary.clear();
+            counter = 0;
+            std::fill (singleBinary.begin(),singleBinary.begin()+8,'0');
+            for(int i = startData;i<byteFile.size();i++) {
+                for (int j = 7; j >= 0; j-- ){
+                    singleBinary.at(j) = (byteFile.at(i) % 2) + '0';
+                    byteFile.at(i) /= 2;
+                }
+                for (int j = 0;j<singleBinary.size();j++) {
+                    // std::cout << singleBinary.at(j);
+                    bufferBinary.push_back(singleBinary.at(j));
+                }
+                // std::cout << std::endl;
+            }
+
+            bufferCitra.clear();
+            buffer.clear();
+            for(int i = 0;i<bufferBinary.size();i++) {
+                std::cout << bufferBinary.at(i);
+                if((i+1)%8 == 0) {
+                    std::cout << std::endl;
+                }
+            }
+            
+
+            // for(int i = 0;i<bufferBinary.size();i++) {
+            //     if ((bufferBinary.at(i) == '0') || (bufferBinary.at(i) == '1')) {
+            //         buffer.push_back(bufferBinary.at(i));
+            //         counter++;
+            //         if (counter == this->getWidth()) {
+            //             counter = 0;
+            //             bufferCitra.push_back(buffer);
+            //             buffer.clear();
+            //         }
+            //     }
+            // }
+            // this->kanal.push_back(bufferCitra);
             break;
         case 2:
             std::cout << "\n";
